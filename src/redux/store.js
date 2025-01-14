0e0580274bbbf01f1a7ad2211237b4644737a6be
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2025-01-14 18:32:37 +0530

    sharedLinks slice and it is configured in store for sharedLinks feature in YourData app feature

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import messageReducer from "../features/messageSlice";
 import conversationsReducer from "../features/conversationsSlice";
+import sharedLinksReducer from "../features/sharedLinksSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     messages: messageReducer,
     conversations: conversationsReducer,
+    sharedLinks: sharedLinksReducer,
   },
 });
